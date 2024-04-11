496b7af0807ab4dcb670bf4e53e18642a26ba3cd
RamosFe <federico.ramos@wazuh.com>
2024-04-10 21:40:55 -0300

    Agregado de multiprocessing.

--- server/common/server.py
+++ server/common/server.py
@@ -1,8 +1,9 @@
+import multiprocessing
 import signal
 import socket
 import logging
 
-from .utils import store_bets, load_bets, has_won
+from .storage import StoreManager
 from .protocol import AgencySocket, SEND_BET_MSG_TYPE, \
     END_SEND_BET_MSG_TYPE, CLOSE_CONNECTION_MSG_TYPE, MAX_AGENCIES, REQUEST_WINNER_MSG_TYPE
 
@@ -15,10 +16,9 @@ class Server:
         self._server_socket.listen(listen_backlog)
 
         self._active_clients = []
-        self._finished_agencies = 0
+        self._store_manager = StoreManager(MAX_AGENCIES)
 
         self._running = True
-        self._available_winners = False
 
         # Define signal handlers
         signal.signal(signal.SIGINT, self.__handle_signal)
@@ -40,80 +40,26 @@ class Server:
         communication with a client. After client with communucation
         finishes, servers starts to accept new connections again
         """
+        multiprocessing.Manager().register('store', StoreManager)
 
-        while self._running:
-            # Check if it is time to get the winners
-            if self._finished_agencies == MAX_AGENCIES and not self._available_winners:
-                logging.info("action: sorteo | result: success")
-                self._available_winners = True
+        with multiprocessing.Pool() as pool:
+            with multiprocessing.Manager() as manager:
+                store = manager.store(MAX_AGENCIES)
 
-            try:
-                client_sock = self.__accept_new_connection()
-                self._active_clients.append(client_sock)
-                self.__handle_client_connection(client_sock)
-                self._active_clients.remove(client_sock)
-            except OSError as err:
-                # The server is shutting down
-                if not self._running:
-                    return
+                while self._running:
+                    try:
+                        client_sock = self.__accept_new_connection()
+                        self._active_clients.append(client_sock)
+                        pool.apply_async(handle_client_connection, (client_sock, store))
 
-                # The server has an internal error, starts shutdown
-                logging.error(f'action: new connection | result: fail | error: {err}')
-                self.__shutdown()
+                    except OSError as err:
+                        # The server is shutting down
+                        if not self._running:
+                            return
 
-    def __handle_client_connection(self, client_sock):
-        """
-        Read message from a specific client socket and closes the socket
-
-        If a problem arises in the communication with the client, the
-        client socket will also be closed
-        """
-        agency_id = None
-
-        try:
-            while True:
-                msg_type = client_sock.recv_msg_type()
-                if msg_type == SEND_BET_MSG_TYPE:
-                    # Receive the message from the client
-                    bets = client_sock.recv_tickets()
-
-                    # Stores bet
-                    store_bets(bets)
-                    for bet in bets:
-                        if not agency_id:
-                            agency_id = bet.agency
-                        logging.info(f'action: apuesta_almacenada | result: success | '
-                                     f'dni: {bet.document} | numero: {bet.number}')
-
-                    # Handle properly the ACK
-                    client_sock.send_ack()
-                elif msg_type == END_SEND_BET_MSG_TYPE:
-                    logging.info(f'action: fin de envio de apuestas | result: success | agency: {agency_id}')
-
-                    # Add 1 to finished agencies
-                    self._finished_agencies += 1
-                elif msg_type == CLOSE_CONNECTION_MSG_TYPE:
-                    logging.info(f'action: mensaje de fin de conexión | result: success | agency: {agency_id}')
-                    break
-                elif msg_type == REQUEST_WINNER_MSG_TYPE:
-                    requested_agency = client_sock.recv_agency_id()
-                    agency_id = requested_agency
-
-                    if self._available_winners:
-                        logging.info(f'action: consulta ganadores | result: success | agency: {requested_agency}')
-                        bets = load_bets()
-                        winners = list(filter(lambda x: has_won(x) and x.agency == requested_agency, bets))
-                        client_sock.send_winners(winners)
-                    else:
-                        logging.info(f'action: consulta ganadores | result: failed | agency: {requested_agency}'
-                                     f' | msg: missing {MAX_AGENCIES - self._finished_agencies} agencies')
-                        client_sock.send_unavailable_winners()
-
-        except OSError as e:
-            logging.error(f"action: receive_message | result: fail | error: {e}")
-        finally:
-            logging.info(f'action: close client socket | result: success | agency: {agency_id}')
-            client_sock.close()
+                        # The server has an internal error, starts shutdown
+                        logging.error(f'action: new connection | result: fail | error: {err}')
+                        self.__shutdown()
 
     def __accept_new_connection(self):
         """
@@ -142,3 +88,57 @@ class Server:
         # Closes the server socket
         self._server_socket.close()
         logging.debug(f'action: close server socket | result: success')
+
+
+def handle_client_connection(client_sock, store):
+    """
+    Read message from a specific client socket and closes the socket
+
+    If a problem arises in the communication with the client, the
+    client socket will also be closed
+    """
+    agency_id = None
+
+    try:
+        while True:
+            msg_type = client_sock.recv_msg_type()
+            if msg_type == SEND_BET_MSG_TYPE:
+                # Receive the message from the client
+                bets = client_sock.recv_tickets()
+
+                # Stores bet
+                store.store_bet(bets)
+                for bet in bets:
+                    if not agency_id:
+                        agency_id = bet.agency
+                    logging.info(f'action: apuesta_almacenada | result: success | '
+                                     f'dni: {bet.document} | numero: {bet.number}')
+
+                # Handle properly the ACK
+                client_sock.send_ack()
+            elif msg_type == END_SEND_BET_MSG_TYPE:
+                logging.info(f'action: fin de envio de apuestas | result: success | agency: {agency_id}')
+
+                # Add 1 to finished agencies
+                store.notify_agency_finished()
+            elif msg_type == CLOSE_CONNECTION_MSG_TYPE:
+                logging.info(f'action: mensaje de fin de conexión | result: success | agency: {agency_id}')
+                return
+            elif msg_type == REQUEST_WINNER_MSG_TYPE:
+                requested_agency = client_sock.recv_agency_id()
+                agency_id = requested_agency
+
+                if store.winners_available():
+                    logging.info(f'action: consulta ganadores | result: success | agency: {requested_agency}')
+                    winners = store.get_winners(requested_agency)
+                    client_sock.send_winners(winners)
+                else:
+                    logging.info(f'action: consulta ganadores | result: failed | agency: {requested_agency}'
+                                     f' | msg: missing agencies')
+                    client_sock.send_unavailable_winners()
+
+    except OSError as e:
+        logging.error(f"action: receive_message | result: fail | error: {e}")
+    finally:
+        logging.info(f'action: close client socket | result: success | agency: {agency_id}')
+        client_sock.close()
